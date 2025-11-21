ab19917186110822f8acad8cc30eb9068a1c13ae
Ahmed MOhamed Fathy <afathe7090@gmail.com>
2025-11-22 00:18:49 +0200

    feat(MoviesUseCase): linting file

--- MoviesList-MVVM-SwiftUI/Modules/MoviesModule/Sources/MoviesModule/Domain/UseCase/MoviesUseCase.swift
+++ MoviesList-MVVM-SwiftUI/Modules/MoviesModule/Sources/MoviesModule/Domain/UseCase/MoviesUseCase.swift
@@ -5,18 +5,20 @@
 //  Created by Menaim on 05/08/2024.
 //
 
-import Foundation
-import MoviesLookups
 import Combine
 import Commons
+import Foundation
+import MoviesLookups
 
 final class MoviesUseCase {
   // MARK: - Private Vars
+
   private var cancellable: Set<AnyCancellable> = []
 
   // MARK: - Dependencies
-  private var moviesRepository: MoviesRepositoryProtocol
-  private var genresRepository: GenreRepositoryProtocol
+
+  private let moviesRepository: MoviesRepositoryProtocol
+  private let genresRepository: GenreRepositoryProtocol
 
   init(
     moviesRepository: MoviesRepositoryProtocol,
@@ -26,28 +28,27 @@ final class MoviesUseCase {
     self.genresRepository = genresRepository
   }
 
-
   // MARK: - Privates
+
   private func convert(_ repositoryItem: [MovieRepositoryModel]?) -> [MovieItem] {
     guard
       let movies = repositoryItem
     else { return [] }
-    return movies.compactMap({ movie in
-        MovieItem(
-          posterPath: movie.posterPath ?? "",
-          title: movie.title ?? "",
-          releaseDate: movie.releaseDate ?? Date(),
-          genres: movie.genreIDs ?? [],
-          id: movie.id ?? 0,
-          voteAverage: movie.voteAverage ?? 0.0,
-          voteCount: movie.voteCount ?? 0,
-          overview: movie.overview ?? ""
-        )
-      })
+    return movies.compactMap { movie in
+      MovieItem(
+        posterPath: movie.posterPath ?? "",
+        title: movie.title ?? "",
+        releaseDate: movie.releaseDate ?? Date(),
+        genres: movie.genreIDs ?? [],
+        id: movie.id ?? 0,
+        voteAverage: movie.voteAverage ?? 0.0,
+        voteCount: movie.voteCount ?? 0,
+        overview: movie.overview ?? ""
+      )
+    }
   }
 
-  private
-  func convert(_ repositoryItem: MoviesRepositoryModel?) -> MoviesItems {
+  private func convert(_ repositoryItem: MoviesRepositoryModel?) -> MoviesItems {
     MoviesItems(
       page: repositoryItem?.page ?? 0,
       movies: convert(repositoryItem?.movies),
@@ -55,14 +56,13 @@ final class MoviesUseCase {
     )
   }
 
-  public
   func convert(_ genres: [GenreRepositoryModel]?) -> [MovieGenre] {
-    genres?.compactMap({ genre in
+    genres?.compactMap { genre in
       MovieGenre(
         id: genre.id ?? 00,
-        name:genre.name ?? "--"
+        name: genre.name ?? "--"
       )
-    }) ?? []
+    } ?? []
   }
 
   func fetchGenres() -> AnyPublisher<[MovieGenre], ModuleError> {
@@ -76,19 +76,20 @@ final class MoviesUseCase {
             ))
           }
         }, receiveValue: { [weak self] response in
-          guard 
+          guard
             let self,
             let response
           else { return }
           promise(.success(convert(response)))
         })
         .store(in: &cancellable)
-      }
+    }
     .eraseToAnyPublisher()
   }
 }
 
 // MARK: - MoviesUseCaseProtocol
+
 extension MoviesUseCase: MoviesUseCaseProtocol {
   func fetchMovies(for currentPage: Int) -> AnyPublisher<MoviesItems, ModuleError> {
     return Future { [weak self] promise in
@@ -107,28 +108,28 @@ extension MoviesUseCase: MoviesUseCaseProtocol {
           ))
         })
         .store(in: &cancellable)
-      }
+    }
     .eraseToAnyPublisher()
   }
 
   func search(with searchText: String, and searchPage: Int) -> AnyPublisher<MoviesItems, ModuleError> {
-      return Future { [weak self] promise in
-        guard let self else { return }
-        moviesRepository.getSearchedMovies(with: searchText, and: searchPage)
-          .sink(receiveCompletion: { result in
-            if case .failure(let error) = result {
-              promise(.failure(
-                ModuleError(error: error)
-              ))
-            }
-          }, receiveValue: { [weak self] response in
-            guard let self else { return }
-            promise(.success(
-              self.convert(response)
+    return Future { [weak self] promise in
+      guard let self else { return }
+      moviesRepository.getSearchedMovies(with: searchText, and: searchPage)
+        .sink(receiveCompletion: { result in
+          if case .failure(let error) = result {
+            promise(.failure(
+              ModuleError(error: error)
             ))
-          })
-          .store(in: &cancellable)
-        }
-      .eraseToAnyPublisher()
+          }
+        }, receiveValue: { [weak self] response in
+          guard let self else { return }
+          promise(.success(
+            self.convert(response)
+          ))
+        })
+        .store(in: &cancellable)
+    }
+    .eraseToAnyPublisher()
   }
 }
